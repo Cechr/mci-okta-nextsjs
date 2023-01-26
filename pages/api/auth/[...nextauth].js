47ce23e599fd1028609c8b469a4d95b2dcd02009
CARLOS EDUARDO CHAN RODRIGUEZ <1076785@horusur.corp>
2023-01-26 12:00:30 -0600

    Se agrega capa de encriptacion y se corrige callback de redireccionamiento

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -44,8 +44,13 @@ export default NextAuth({
     },
     callbacks: {
         redirect: async ({ url, baseUrl }) => {
+            // Setting the new baseUrl
             baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-            return baseUrl;
+            // Allows relative callback URLs
+            if (url.startsWith("/")) return `${baseUrl}${url}`
+            // Allows callback URLs on the same origin
+            else if (new URL(url).origin === baseUrl) return url
+            return baseUrl
         },
         jwt: async ({ token, user }) => {
             if (user) {
